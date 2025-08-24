28276742934391ed4c360811465d267e547709d4
hxkkry <1504941291@qq.com>
2025-08-24 11:57:41 +0800

    跳过微信支付功能

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -3,6 +3,8 @@ package com.sky.mapper;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.time.LocalDateTime;
+
 @Mapper
 public interface OrderMapper {
     /**
@@ -10,4 +12,26 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+
+    /**
+     * 根据订单号查询订单
+     * @param outTradeNo
+     * @return
+     */
+    Orders getByNumber(String outTradeNo);
+
+    /**
+     * 更新订单数据
+     * @param orders
+     */
+    void update(Orders orders);
+
+    /**
+     * 更新订单状态
+     * @param orderStatus
+     * @param orderPaidStatus
+     * @param checkOutTime
+     * @param id
+     */
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime checkOutTime, Long id);
 }
